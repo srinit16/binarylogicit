63ebd4cb1b39f52e5284ff8767931f0a995fb470
srinit16 <srinit16@gmail.com>
2023-05-16 15:31:49 -0400

    pushing stockapp

---
+++ StockApp/src/main/java/org/thewalking/stock/StockAppApplication.java
@@ -0,0 +1,14 @@
+package org.thewalking.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAppApplication.class, args);
+		
+	}
+
+}
---
+++ StockApp/src/test/java/org/thewalking/stock/StockAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thewalking.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

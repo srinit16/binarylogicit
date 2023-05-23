a54f85b5cc64652085bfde6b506b2c7d02263b45
srinit16 <srinit16@gmail.com>
2023-05-23 11:59:27 -0400

    changed many things

--- StockApp/src/main/java/org/thewalking/stock/controller/ProductController.java
+++ StockApp/src/main/java/org/thewalking/stock/controller/ProductController.java
@@ -1,16 +1,24 @@
 package org.thewalking.stock.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.thewalking.stock.entities.Product;
+import org.thewalking.stock.entities.ProductCategory;
 import org.thewalking.stock.service.ProductService;
+import org.thewalking.stock.util.Message;
 
-@RestController
+@RestController @CrossOrigin
 @RequestMapping("/api/products")
 public class ProductController {
 		@Autowired
@@ -25,4 +33,22 @@ public class ProductController {
 			}
 			return p;
 		}
+		
+		@GetMapping("/findByCategory")
+		public List<Product> findByCategoryId(@RequestParam("id") long cid){
+			return ps.findAll(cid);
+			
+		}
+		
+		@GetMapping("/category/all")
+		public List<ProductCategory> ListCategory(){
+			return ps.findAll();
+			
+		}
+		
+		@PostMapping
+		public Message save(@RequestBody Product p) {
+					return ps.createProduct(p);
+		}
+		
 }
--- StockApp/src/main/java/org/thewalking/stock/service/ProductService.java
+++ StockApp/src/main/java/org/thewalking/stock/service/ProductService.java
@@ -1,18 +1,50 @@
 package org.thewalking.stock.service;
 
+import java.util.List;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.thewalking.stock.dao.ProductCategoryDao;
 import org.thewalking.stock.dao.ProductDao;
 import org.thewalking.stock.entities.Product;
+import org.thewalking.stock.entities.ProductCategory;
+import org.thewalking.stock.util.Message;
 
 @Service
 public class ProductService {
 		@Autowired
 		ProductDao pdao;
 		
+		@Autowired
+		ProductCategoryDao dao;
+		
 		public Optional<Product> find(long id) {
 				return pdao.findById(id);
 		}
+		
+		public List<Product> findAll(long id) {
+			return pdao.findProductByCategoryId(id);
+	    }
+		
+		public List<ProductCategory> findAll() {
+			
+			return dao.findAll();
+	    }
+		
+		@Transactional
+		public Message createProduct(Product p) {
+				Message m1=new Message();
+				try {
+					Product p1=pdao.save(p);
+					m1.setInfo("Product with Id ="+p1.getId()+ " is succcessfully added");
+				}catch(Exception ex) {
+					//throw new RuntimeException();
+					m1.setInfo("Error occured while saving Product, Details :"+ex.toString());
+				}
+				return m1;
+		}
+		
 }

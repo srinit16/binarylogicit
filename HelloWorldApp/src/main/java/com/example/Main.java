bb0d33bae43ed8b0b82077b3c41c4fa2922e6bfa
srinit16 <srinit16@gmail.com>
2023-07-26 11:07:06 -0400

    Spring Annotation

--- HelloWorldApp/src/main/java/com/example/Main.java
+++ HelloWorldApp/src/main/java/com/example/Main.java
@@ -10,7 +10,7 @@ public class Main {
 	public static void main(String[] args) {
 	
 		ApplicationContext context= new ClassPathXmlApplicationContext("beans.xml");
-		HelloWorld h1=(HelloWorld) context.getBean("helloworld");
+		HelloWorld h1=(HelloWorld) context.getBean(HelloWorld.class);
 		System.out.println("Hello "+h1.getName());
 		//System.out.println("\nAddress\n"+h1.getAdd().toString());
 		System.out.println("\nAddress\n"+h1.getAdd());
--- HelloWorldApp/src/main/java/com/example/beans/HelloWorld.java
+++ HelloWorldApp/src/main/java/com/example/beans/HelloWorld.java
@@ -1,14 +1,20 @@
 package com.example.beans;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import lombok.Getter;
 import lombok.Setter;
 
 //@Setter @Getter
+
 public class HelloWorld {  // POJO
 	private String name;
-	
+	@Autowired
 	private Address add;
-
+	
+	
+	
+	
 	public String getName() {
 		return name;
 	}
